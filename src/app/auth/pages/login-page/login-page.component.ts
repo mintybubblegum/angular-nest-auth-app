23b54e6cd7ca257faf453569349d2200dc6c94c2
[mintybubblegum] <anamaria.tz@hotmail.com>
2023-08-02 12:06:06 +0200

    login finished + validators - exercise 356

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,9 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent {
+
+  private fb = inject( FormBuilder );
+
+  public myForm: FormGroup = this.fb.group({
+    email: ['', [ Validators.required, Validators.email ]],
+    password: ['', [ Validators.required, Validators.minLength(6) ]],
+  })
+
+  isNotValidField(field: string): boolean | null {
+    return this.myForm.controls[field].errors
+    && this.myForm.controls[field].touched;
+  }
+
+  getFieldError(field: string): string | null {
+
+    if ( !this.myForm.controls[field] ) return null;
+
+    const errores = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errores)) {
+      switch( key ) {
+        case 'required':
+          return 'Field required';
+
+        case 'email':
+          return 'Please enter a valid email.';
+
+        case 'minlength':
+          return `This field needs a minimum of ${ errores['minlength'].requiredLength } characters.`;
+      }
+    }
+
+    return null;
+  }
+
+  login() {
+    console.log( this.myForm.value );
+    
+  }
 
 }
