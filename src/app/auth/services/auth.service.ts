e6cca04a743bb6cad980d14117601cb91ebc5d0c
[mintybubblegum] <anamaria.tz@hotmail.com>
2023-08-10 13:01:32 +0200

    post, login and fixing CORS enabling it in localStorage - exercise 360

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,9 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject, signal } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Injectable, computed, inject, signal } from '@angular/core';
+import { Observable, map, of, tap } from 'rxjs';
 import { environment } from 'src/environments/environments';
 
-import { AuthStatus, User } from '../interfaces';
+import { AuthStatus, LoginResponse, User } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -16,10 +16,33 @@ export class AuthService {
   private _currentUser = signal<User|null>(null);
   private _authStatus = signal<AuthStatus>( AuthStatus.checking );
 
+  //! Al mundo exterior - para que nadie fuera del servicio pueda cambiarlo
+  public currentUser = computed( () => this._currentUser() );
+  public authStatus = computed( () => this._authStatus() );
+
+
   constructor() { }
 
   login( email: string, password: string ): Observable<boolean> {
 
+    const url = `${ this.baseUrl }/auth/login`;
+    const body = { email, password };
+
+    return this.http.post<LoginResponse>( url, body )
+      .pipe(
+        tap( ({ user, token }) => {
+          this._currentUser.set( user );
+          this._authStatus.set( AuthStatus.authenticated );
+          localStorage.setItem('token', token);
+
+          console.log({ user, token });
+          
+        }),
+
+        map( () => true )
+
+        //Todo: errores
+      )
 
     return of(true);
   }
