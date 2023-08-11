11932a5e0e414e169b8f1495f30fc1786e6409f1
[mintybubblegum] <anamaria.tz@hotmail.com>
2023-08-11 13:44:28 +0200

    service to verify access token - exercise 364

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, computed, inject, signal } from '@angular/core';
 import { Observable, catchError, map, of, tap, throwError } from 'rxjs';
 import { environment } from 'src/environments/environments';
 
-import { AuthStatus, LoginResponse, User } from '../interfaces';
+import { AuthStatus, CheckTokenResponse, LoginResponse, User } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -44,7 +44,32 @@ export class AuthService {
         
         )
       )
+  }
 
-    return of(true);
+  checkAuthStatus():Observable<boolean> {
+
+    const url   = `${ this.baseUrl }/auth/check-token`;
+    const token = localStorage.getItem('token');
+
+    if ( !token ) return of(false);
+
+    const headers = new HttpHeaders()
+      .set('Authorization', `Bearer ${ token }`);
+
+      return this.http.get<CheckTokenResponse>(url, { headers })
+        .pipe(
+          map( ({ token, user }) => {
+            this._currentUser.set( user );
+            this._authStatus.set( AuthStatus.authenticated );
+            localStorage.setItem('token', token);
+            
+            return true;
+          }),
+          //Error
+          catchError(() => {
+            this._authStatus.set( AuthStatus.notAuththenticated );
+            return of(false);
+          })
+        );
   }
 }
