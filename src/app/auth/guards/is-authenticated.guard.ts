fd372ca7947c67ac7ec9144dc57c96ac2ba14151
[mintybubblegum] <anamaria.tz@hotmail.com>
2023-08-11 10:57:53 +0200

    implenting logic of functional guards - exercise 363

--- src/app/auth/guards/is-authenticated.guard.ts
+++ src/app/auth/guards/is-authenticated.guard.ts
@@ -1,13 +1,20 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { inject } from '@angular/core';
+import { AuthStatus } from '../interfaces';
 
 export const isAuthenticatedGuard: CanActivateFn = (route, state) => {
   
   
-  console.log('isAuthenticated');
-  console.log({route, state});
+  const authService = inject( AuthService );
+  const router      = inject( Router );
+
+  if ( authService.authStatus() === AuthStatus.authenticated ){
+    return true;
+  }
   
-  
-  
-  
-  return true;
+  // const url = state.url;
+  // localStorage.setItem('url', url);
+  router.navigateByUrl('/auth/login')
+  return false;
 };
