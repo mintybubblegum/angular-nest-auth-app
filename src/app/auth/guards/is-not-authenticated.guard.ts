c4c0fbac46d4e1a4b671e23746b7f9fac991bc31
[mintybubblegum] <anamaria.tz@hotmail.com>
2023-08-14 11:35:14 +0200

    isNotAuthenticatedGuard - exercise 367

--- src/app/auth/guards/is-not-authenticated.guard.ts
+++ src/app/auth/guards/is-not-authenticated.guard.ts
@@ -3,21 +3,15 @@ import { AuthService } from '../services/auth.service';
 import { inject } from '@angular/core';
 import { AuthStatus } from '../interfaces';
 
-export const isAuthenticatedGuard: CanActivateFn = (route, state) => {
+export const isNotAuthenticatedGuard: CanActivateFn = (route, state) => {
   
   const authService = inject( AuthService );
   const router      = inject( Router );
 
   if ( authService.authStatus() === AuthStatus.authenticated ){
-    return true;
+    router.navigateByUrl('/dashboard')
+    return false;
   }
 
-  // if ( authService.authStatus() === AuthStatus.checking ) {
-  //   return false;
-  // }
-  
-  // const url = state.url;
-  // localStorage.setItem('url', url);
-  router.navigateByUrl('/auth/login')
-  return false;
+  return true;
 };
